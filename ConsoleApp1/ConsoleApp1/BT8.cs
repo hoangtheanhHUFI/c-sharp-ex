fa08d20463c0f85841fe4c6d20b8c73b4dd7b61b
Hoang The Anh <2001202008@hufi.edu.vn>
2022-06-24 16:45:52 +0700

    update

--- ConsoleApp1/ConsoleApp1/BT8.cs
+++ ConsoleApp1/ConsoleApp1/BT8.cs
@@ -22,7 +22,7 @@ namespace BaiTap
         }
         public string Unit //Set property cho Unit, chi nhan 4 gia tri
         {
-            get { return unit; }
+            get { return unit.ToLower(); }
             set
             {
                 if(value=="ket")
@@ -43,7 +43,7 @@ namespace BaiTap
                 }
                 else
                 {
-                    Console.WriteLine("Du lieu nhap vao sai !");
+                    Console.WriteLine("Du lieu nhap vao sai !(Unit)");
                     unit = "Invalid";
                 }
             }
@@ -77,28 +77,65 @@ namespace BaiTap
          //-------------------------Ham khoi tao-------------------
         public NuocGiaiKhat()
         {
-            Name= Unit = "";
+            Name=  "";
             Amount = 0;
             Prices = 0;
         }
         public NuocGiaiKhat(string name, string unit, int amount, double prices)
         {
-            this.Name = name;
-            this.Unit = unit;
-            this.Amount = amount;
-            this.Prices = prices;
+            this.name = name;
+            this.unit = unit;
+            this.amount = amount;
+            this.prices = prices;
             
         }
         public NuocGiaiKhat(NuocGiaiKhat a)
         {
-            Name = a.Name;
-            Unit = a.Unit;
-            Amount = a.Amount;
-            Prices = a.Prices;
+            Name = a.name;
+            Unit = a.unit;
+            Amount = a.amount;
+            Prices = a.prices;
+        }
+        public void input()
+        {
+            Console.WriteLine("Nhap vao ten nuoc giai khat");
+            name = Console.ReadLine();
+            Console.WriteLine("Nhap vao don vi tinh");
+            unit = Console.ReadLine();
+            Console.WriteLine("Nhap vao don so luong");
+            amount = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Don gia ");
+            prices = double.Parse(Console.ReadLine());
+        }
+
+        //-----------------------Ham Huy------------------------------
+        ~NuocGiaiKhat()
+        {
+
         }
         //-----------------------------Ham Xu Li-------------------------
-
+        public double total()
+        {
+            if (unit == "ket" || unit == "thung")
+                return (amount * prices) + (amount * prices * vat_tax);
+            else if (unit == "chai")
+                return (amount * (prices / 20)) + (amount * (prices / 20) * vat_tax);
+            else if (unit == "lon")
+                return (amount*(prices/24))+(amount*(prices/24*vat_tax));
+            else
+                return 0;
+        }
     }
-    class program { }
+    class program 
+    {
+        static void Main(string[] agrs)
+        {
+            NuocGiaiKhat a = new NuocGiaiKhat();
+            a.input();
+            Console.WriteLine("{0} {1} {2} tri gia {3}",a.Amount,a.Unit,a.Name,a.total());
+            
+    
+        }
+    }
 
 }
